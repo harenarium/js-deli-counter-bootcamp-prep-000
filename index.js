b7d8471de76aacf43fee2670a4e9d344b6f6ba33
harenarium <carolinel2610@gmail.com>
2017-11-03 03:08:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine,name){ //katzDeliLine= current line array?
+  katzDeliLine.push("name")
+  return katzDeliLine.length +1
+}
+
+function nowServing(){
+  if(katzDeliLine.length>0){
+    return katzDeliLine[0]
+  }else{
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(){
+  if(katzDeliLine.length>0){
+    var string = "The line is currently: "//1. Ada, 2. Grace"
+    for(var i=0;i<katzDeliLine.length;i++){
+      string = string + (i=0 ? `${i}. ${katzDeliLine[i]}` : `, ${i}. ${katzDeliLine[i]}`)
+    }
+  }else{
+    return "The line is currently empty."
+  }
+  
+}
