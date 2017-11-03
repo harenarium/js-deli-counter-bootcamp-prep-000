9a0cc8737738ea53c09dfd7abea84a9f0d19c66f
harenarium <carolinel2610@gmail.com>
2017-11-03 03:19:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine,name){ //katzDeliLine= current line array?
   katzDeliLine.push(name)
-  var number =katzDeliLine.length 
+  var number =katzDeliLine.length
   return `Welcome, ${name}. You are number ${number} in line.`
 }
 
 function nowServing(katzDeliLine){
   if(katzDeliLine.length>0){
-    return `Currently serving ${katzDeliLine[0]}.`
     katzDeliLine.shift(1)
+    return `Currently serving ${katzDeliLine[0]}.`
   }else{
     return "There is nobody waiting to be served!"
   }
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length>0){
     var string = "The line is currently: "//1. Ada, 2. Grace"
     for(var i=0;i<katzDeliLine.length;i++){
-      string = string + (i=0 ? `${i}. ${katzDeliLine[i]}` : `, ${i}. ${katzDeliLine[i]}`)
+      string = string + (i=0 ? `${i+1}. ${katzDeliLine[i]}` : `, ${i+1}. ${katzDeliLine[i]}`)
     }
     return string
   }else{
